85ac55d26b87e3837aea2ea72b74618800ea8aa7
Freddy220103 <alfredo220103@gmail.com>
2025-04-30 18:06:47 -0600

    Initial commit: Estructura básica del paquete ROS 2

---
+++ README.md
@@ -0,0 +1,2 @@
+# ServoYSirvo_nav2_puzzlebot
+Repositorio para el proyecto de navegación autónoma con PuzzleBot y ROS 2 Nav2
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+package_name = 'NombreDelEquipo_nav2_puzzlebot'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/simulation_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/navigation_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/slam_launch.py']),
+        ('share/' + package_name + '/config', ['config/nav2_params.yaml']),
+        ('share/' + package_name + '/config', ['config/mapper_params_online_async.yaml']),
+        ('share/' + package_name + '/rviz', ['config/rviz/nav.rviz']),
+        ('share/' + package_name + '/rviz', ['config/rviz/slam.rviz']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Integrante 1, Integrante 2, Integrante 3',
+    maintainer_email='email1@equipo.com, email2@equipo.com, email3@equipo.com',
+    description='Paquete de navegación autónoma para PuzzleBot con Nav2',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
