d6d56f0ec655560197d7e47018ef62123b5128a0
Victorn65 <a01562528@tec.mx>
2025-05-14 00:16:31 -0600

    Modificacion de localisation.py

--- build/ServoYSirvo_nav2_puzzlebot/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/victorn65/ros2_ws/src/ServoYSirvo_nav2_puzzlebot/install/ServoYSirvo_nav2_puzzlebot'
